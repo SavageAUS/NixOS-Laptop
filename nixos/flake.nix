89afb1142b6c57d284f1cd9d887586e32c1457ca
SavageAUS <shane.r.scott1981@gmail.com>
2025-12-27 21:44:01 +1030

    Add files via upload

---
+++ nixos/flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "Hyprland on Nixos";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-25.11";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, home-manager, ... }: {
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      modules = [
+        ./configuration.nix
+        home-manager.nixosModules.home-manager
+        {
+          home-manager = {
+            useGlobalPkgs = true;
+            useUserPackages = true;
+            users.shane = import ./home.nix;
+            backupFileExtension = "backup";
+          };
+        }
+      ];
+    };
+  };
+}
